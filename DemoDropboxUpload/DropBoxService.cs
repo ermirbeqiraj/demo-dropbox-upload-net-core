16385ab47bbf047d7bd371961b647615d0e8cefa
ermir beqiraj <ermirbeqiraj@hotmail.com>
2018-12-20 15:04:45 +0100

    Upload a file in dropbox from dotnet core console application

---
+++ DemoDropboxUpload/DropBoxService.cs
@@ -0,0 +1,27 @@
+﻿using Dropbox.Api;
+using Dropbox.Api.Files;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DemoDropboxUpload
+{
+    public class DropBoxService
+    {
+        private string ApiToken { get; set; }
+        public DropBoxService(string token)
+        {
+            ApiToken = token;
+        }
+
+        public async Task Upload(string remoteFileName, string localFilePath)
+        {
+            using (var dbx = new DropboxClient(this.ApiToken))
+            {
+                using (var fs = new FileStream(localFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    await dbx.Files.UploadAsync($"/{remoteFileName}", WriteMode.Overwrite.Instance, body: fs);
+                }
+            }
+        }
+    }
+}
---
+++ DemoDropboxUpload/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace DemoDropboxUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dropboxApiToken = "<DROPBOX_TOKEN_HERE>";
+            var bakupFolderPath = "<YOUR ABSOLUTE PATH TO THE BACKUP FOLDER IN HERE>";
+            try
+            {
+                var dateNowString = System.DateTime.Now.ToString("yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+                var backupName = $"<YOUR_DB_NAME>_db_bak{dateNowString}.sql.gz";
+                var localFilePath = Path.Combine(bakupFolderPath, backupName);
+
+                if (File.Exists(localFilePath))
+                {
+                    var dbs = new DropBoxService(dropboxApiToken);
+                    Task.Run(async () =>
+                    {
+                        await dbs.Upload(backupName, localFilePath);
+                    }).Wait();
+                    File.Delete(localFilePath);
+                }
+                else
+                {
+                    // WARN
+                }
+            }
+            catch (System.Exception ex)
+            {
+                while (ex.InnerException != null)
+                    ex = ex.InnerException;
+
+                // WARN
+            }
+        }
+    }
+}
